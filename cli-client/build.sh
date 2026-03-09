adb5cf4ee648f1324b6eca5af6f9abc68d03d848
dertfin0 <dertfin@gmail.com>
2026-03-09 11:56:41 +0300

    client: fix .sh script

---
+++ cli-client/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pip install -r requirements.txt
+pyinstaller \
+--hidden-import textual \
+--hidden-import python-dotenv --collect-submodules dotenv \
+--hidden-import httpx \
+--onefile \
+--noupx \
+--icon icon.ico \
+main.py
